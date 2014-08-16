359ce06bf00fd42804a810056762ae19c31b1925
Nic Botha <nic.botha@gmail.com>
2014-08-16 10:40:16 +1000

    Cleanup

--- src/main/java/au/com/sap/mcc/timeteam/dao/TaskRepository.java
+++
@@ -1,9 +0,0 @@
-package au.com.sap.mcc.timeteam.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import au.com.sap.mcc.timeteam.model.Task;
-
-public interface TaskRepository extends CrudRepository<Task, String>{
-
-}
